d7ed6473f9b68e3be5bac2651949fd5ae06726c7
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-11-01 03:21:48 -0700

    Transparent Navbar Component for Homepage
    Transparent Navbar Component for Homepage

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router-dom";
+import Navbar from "../../Navbar/Navbar";
+export default function ErrorPage() {
+  const error = useRouteError();
+  return (
+    <div className="">
+      <Navbar />
+      <div className="flex flex-col justify-center items-center pt-24 pb-24 font-Regular">
+        <h1 className="font-Bold text-6xl">Oops!</h1>
+        <p className="text-xl text-slate-600 py-8 text-center">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-amber-500 underline px-12 pt-0 pb-3 rounded-xl text-xl text-center self-center">
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </div>
+  );
+}
