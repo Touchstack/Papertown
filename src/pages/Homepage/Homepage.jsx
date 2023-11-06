cb69bbfba3d9d6239e15ec287c79beceac35288c
Eugene Dumoga <eugenedumoga@gmail.com>
2023-11-06 11:55:58 +0000

    Change to Navbar Update test

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
     <div>
       {/* <Navbar /> */}
       <SignUpNavbar />
-      <h1 className="my-8 text-center">HomePAGE tEXT</h1>
+      <h1 className="my-8 text-center">Chnage to Navbar Branch</h1>
     </div>
   );
 };
