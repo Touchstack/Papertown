4fad83ae111e12ab8dbfc0a09b5e146f09b9088d
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-11-06 11:40:17 +0000

    @EugeneSantaclausian

--- src/pages/Slidershow/Slidershow.jsx
+++ src/pages/Slidershow/Slidershow.jsx
@@ -5,6 +5,8 @@ import { sliderData } from "../../../Component/sliderData";
 import "../../Style/Slidershow.css";
 import iamgeBD from "../..//assets/Images/image.png";
 import Text from "../../../Component/Text";
+import Icon1 from "../..//assets/Images/icon1.png";
+
 function Slidershow({ slides }) {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
@@ -29,7 +31,39 @@ function Slidershow({ slides }) {
       <section className="slider">
         <HiChevronLeft className="slider__leftArrow" onClick={prevSlider} />
         <HiChevronRight className="slider__rightArrow" onClick={nextSlider} />
-        <img src={iamgeBD} alt="" className="slider__image" />
+        <div>
+          <div className="text-left">
+            <h1
+              className="text-red-500 
+				text-3xl font-bold"
+            >
+              Our Partners
+            </h1>
+          </div>
+          <div className="py-4 flex justify-center">
+            <div
+              className="block p-6 max-w-sm 
+					bg-white 
+					rounded-lg border 
+					border-gray-200 shadow-md"
+            >
+              <img src={Icon1} alt="" />
+              <h1
+                className="mb-2 text-3xl
+					font-bold 
+					text-black 
+					dark:text-white"
+              >
+                Android
+              </h1>
+              <p class="font-normal text-white">
+                A short description of how the Papertown Imaginarium partners
+                with this compnay
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <img src={iamgeBD} alt="" className="slider__image" /> */}
       </section>
     </div>
   );
