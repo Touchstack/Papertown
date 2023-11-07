54a955074c2c226935c58ba1a930ce45e936dba9
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-11-07 12:33:33 +0000

    @EugeneSantaclausian  Footer Compinent Done

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SignUpNavbar from "../../Navbar/SignUpNavbar";
 import Footer from "../../Component/Footer";
+import Slidershow from "../Slidershow/Slidershow";
 
 const Homepage = () => {
   return (
-    <div className="h-screen">
+    <div className=" bg-gradient-to-r from-purple-600 via-pink-500 to-purple-600 font-Medium h-full">
       <SignUpNavbar />
+
       <Footer />
     </div>
   );
