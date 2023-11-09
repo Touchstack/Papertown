1cf25d41dc779a1f80b102d29c48afb23d3f6afe
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-11-09 12:43:30 +0000

    @EugeneSantaclausian

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import SignUpNavbar from "../../Navbar/SignUpNavbar";
 import Slidershow from "../Slidershow/Slidershow";
 import Footer from "../FooterPage/Footer";
+// import Circle from "../../Component/Circle";
 
 const Homepage = () => {
   return (
-    <div className="bg-gradient-to-r from-purple-600 via-pink-500 to-purple-600 font-Medium h-screen">
+    <div className="">
+      {/* bg-gradient-to-r from-purple-600 via-pink-500 to-purple-600 font-Medium h-screen */}
+      {/* <Circle> */}
       <SignUpNavbar />
       <Slidershow />
+
       <Footer />
+      {/* </Circle> */}
     </div>
   );
 };
