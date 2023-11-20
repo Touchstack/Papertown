85af5ea2b72a9eef23e0499f6bb05f91e3df3b74
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-11-20 19:35:42 +0000

    made  responsive.

--- src/pages/PublicationsPage/SinglePublicationData.jsx
+++ src/pages/PublicationsPage/SinglePublicationData.jsx
@@ -1,7 +1,7 @@
 import PublicationsRowData from "../../Component/PublicationsRowData/PublicationsRowData";
 import GeneralNav from "../../Navbar/GeneralNav"
 import Footer from '../FooterPage/Footer';
-import { useLocation } from 'react-router-dom';
+import { useLocation, } from 'react-router-dom';
 import { HiMiniChevronLeft, HiMiniChevronRight } from "react-icons/hi2";
 import TalkToUs from "../../Component/PublicationsRowData/TalkToUs";
 
@@ -15,11 +15,11 @@ const SinglePublicationData = () => {
             <GeneralNav bgShadow="shadow" btnColor="black" bgColor="transparent" color="black" />
          </div>
 
-         <div className="flex flex-col md:flex-row py-40 px-40 gap-[100px]">
+         <div className="flex flex-col md:flex-row md:py-40 py-20 lg:px-40 px-5 gap-[100px]">
 
             {/*Card*/}
-            <div className='max-w-sm rounded-[15px] border px-[20px] pt-[25px] bg-[#F5F5F5]'>
-            <div className='max-w-sm rounded-[15px_15px_0px_0px] border '>
+            <div className='max-w-md rounded-[15px] border px-[20px] pt-[25px] bg-[#F5F5F5]'>
+            <div className='max-w-md rounded-[15px_15px_0px_0px] border '>
                 <img src={state.item.image} alt='' />
             </div>
             </div>
@@ -28,7 +28,7 @@ const SinglePublicationData = () => {
             <div className="flex flex-col">
                <h1 
                 style={{ lineHeight: '100%', letterSpacing: '2.16px' }}
-               className="text-[72px] font-bold w-[611px]">{state.item.topic}
+               className="md:text-[72px] text-[24px] font-[900] md:w-[611px]">{state.item.topic}
                </h1>
 
                <div className="mt-5">
@@ -36,25 +36,25 @@ const SinglePublicationData = () => {
                </div>
 
                <div className="mt-5">
-                <p className="w-[530px] font-Bold">
+                <p className="md:w-[530px] font-Bold">
                 {state.item.content}
                 </p>
 
-                <p className="w-[530px]">
+                <p className="md:w-[530px] mt-2">
                 {state.item.content}
                 </p>
                </div>
 
                {/* Description section */}
-                <div className="flex flex-col mt-7 gap-2">
-                    <div className="flex flex-row gap-10 text-[#667085]">
+                <div className="flex flex-col mt-7 gap-2 flex-wrap">
+                    <div className="flex flex-row md:gap-10 gap-5 text-[#667085]">
                         <p>Pages</p>
                         <p>Date of publication</p>
                         <p>Languauge</p>
                         <p>ISBN</p>
                     </div>
 
-                    <div className="flex flex-row gap-[3.8rem] font-bold">
+                    <div className="flex flex-row md:gap-[3.8rem] gap-10 font-bold">
                         <p>{state.item.pages}</p>
                         <p>{state.item.dop}</p>
                         <p>{state.item.language}</p>
@@ -64,7 +64,7 @@ const SinglePublicationData = () => {
             </div>
          </div>
 
-          <div className="px-40 pb-20">
+          <div className="md:px-40 px-5 pb-20">
              <div className="flex flex-row items-center justify-between">
                <h1 className="text-[42px] font-Bold hidden md:flex mr-[10rem]">Other related stories</h1>
                <h1 className="text-[42px] font-Bold md:hidden">Other Publication</h1>
