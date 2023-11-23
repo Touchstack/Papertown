693a18ca64cf464aabc71c57b32f53f2e66518b0
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-11-23 17:50:35 +0000

    Marquee text fixed @oddoye

--- src/Component/CallToAction/CallToAction.jsx
+++ src/Component/CallToAction/CallToAction.jsx
@@ -1,8 +1,6 @@
-import LeftArrow from "../../assets/Images/iconLeft.png"
-import RightArrow from "../../assets/Images/iconRight.png"
-import RightImage from "../../assets/Images/unsplash_O5EMzfdxedg.png"
-
-
+import LeftArrow from "../../assets/Images/iconLeft.png";
+import RightArrow from "../../assets/Images/iconRight.png";
+import RightImage from "../../assets/Images/unsplash_O5EMzfdxedg.png";
 
 const CallToAction = () => {
   return (
@@ -10,17 +8,18 @@ const CallToAction = () => {
       <div className="w-[1241px] h-[548px] grid lg:grid-cols-2 grid-cols-1 gap-8 bg-white lg:mx-24 md:mx-16 mx-12 rounded-[43px] items-center mt-10">
         <div className=" translate-x-6 flex-col justify-center py-4 px-10 items-start inline-flex ">
           <h1 className=" lg:text-7xl md:text-5xl text-3xl text-black font-Bold mt-4 tracking-wider items-center ">
-            Technovation 
-            <p>announces</p> 
+            Technovation
+            <p>announces</p>
             <p>finalists</p>
           </h1>
-          <p className="text-black text-lg mb-2 items-center text-justify font-Regular"><br/>
-           It was popularised in the 1960s with the release of Letraset sheets <p>containing Lorem Ipsum passages, and more recently</p><p> with desktop publishing software.</p>
+          <p className="text-black text-lg mb-2 items-center text-justify font-Regular">
+            <br />
+            It was popularised in the 1960s with the release of Letraset sheets{" "}
+            <p>containing Lorem Ipsum passages, and more recently</p>
+            <p> with desktop publishing software.</p>
           </p>
           <button
             className="bg-[#099B96]
-            
-    
            hover:bg-[#099B96] cursor-pointer 
           place-self-start mt-8 font-Bold 
           lg:mb-16 space-y-4 sm:flex-row sm:justify-center 
@@ -33,25 +32,20 @@ const CallToAction = () => {
           >
             Read more
           </button>
-         
         </div>
         <div className=" relative object-cover w-full h-auto mx-auto translate-x-5 inset-0 z-0">
-        <img src={RightImage} />
-        <div className="z-10 absolute right-1 bottom-5 gap-5 flex">
-        <button className="bg-translate text-white rounded-full">
-          <img src={LeftArrow} alt="Previous image"/>
-        </button>
-        <button className="bg-translate text-white rounded-full">
-        <img src={RightArrow} alt="Next image"/>
-        </button>
-        </div>
-        </div>
-       
-       
-        
+          <img src={RightImage} />
+          <div className="z-10 absolute right-1 bottom-5 gap-5 flex">
+            <button className="bg-translate text-white rounded-full">
+              <img src={LeftArrow} alt="Previous image" />
+            </button>
+            <button className="bg-translate text-white rounded-full">
+              <img src={RightArrow} alt="Next image" />
+            </button>
+          </div>
         </div>
       </div>
-    
+    </div>
   );
 };
 
--- src/Component/MarqueeText.jsx
+++ src/Component/MarqueeText.jsx
@@ -1,56 +1,56 @@
 const MarqueeText = () => {
   return (
-    <div className="flex flex-col text-justify">
+    <div className="overflow-hidden flex flex-col text-justify  md:mt-10 mt-[340px]">
       <div className="marquee1 font-Bold text-7xl text-zinc-100 leading-72px tracking-widest ">
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
-        </span> 
-        <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <span>
-           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .  
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
+        </span>
+        <span>
+          WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
         </span>
         <p className="marquee2">
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .  
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .  
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .  
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .  
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .  
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
           <span>
-             WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
+            WRITE <span className="text-yellow-500"> SUBMIT</span> READ .
           </span>
-          </p>
-        </div>
+        </p>
       </div>
+    </div>
   );
 };
 
