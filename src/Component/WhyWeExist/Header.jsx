9bf325b637d2e01f3750f98f776f6d2060ebf251
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-11-23 04:15:43 -0800

    Update to why we exist
    Update to why we exist

--- src/Component/WhyWeExist/Header.jsx
+++ src/Component/WhyWeExist/Header.jsx
@@ -2,15 +2,15 @@ import icon from "../../assets/Images/base.svg";
 
 const Header = () => {
   return (
-    <div className="bg-[#FFD8FF] py-12">
-      <div className="container mx-auto mt-8">
-        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 lg:mb-40 md:mb-40 sm:mb-80 mb-80">
+    <div className="bg-[#FFD8FF] py-10">
+      <div className="container mx-auto">
+        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 lg:mb-20 md:mb-20 sm:mb-80 mb-80">
           {/* Left Col */}
-          <h3 className="font-Bold text-[#000] text-6xl">
+          <h3 className="font-Bold text-[#000] lg:text-7xl md:text-6xl sm:text-6xl text-6xl mt-8 text-center">
             Welcome to Papertown - Read, Write, Submit
           </h3>
           {/* Right Col*/}
-          <div className="container mx-auto relative">
+          <div className="container mx-auto relative mt-8">
             <div className="flex justify-center items-center">
               <div className="bg-white rounded-3xl shadow-shadow absolute top-4 left-0 w-full h-full z-10">
                 <div className="text-[#393939] text-lg mt-7 pl-4 pr-4 m-4">
