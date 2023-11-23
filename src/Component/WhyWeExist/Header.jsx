79d0d3443eda9917441bb1a4ec1dccc8eb9e2094
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-11-23 08:54:38 -0800

    Update Header on why we exist page
    Update Header on why we exist page

--- src/Component/WhyWeExist/Header.jsx
+++ src/Component/WhyWeExist/Header.jsx
@@ -10,12 +10,17 @@ const Header = () => {
             Welcome to Papertown - Read, Write, Submit
           </h3>
           {/* Right Col*/}
-          <div className="container mx-auto relative mt-8">
+          {/* <!-- First Card --> */}
+          <div className="container mx-auto relative py-4">
             <div className="flex justify-center items-center">
-              <div className="bg-white rounded-3xl shadow-shadow absolute top-4 left-0 w-full h-full z-10">
+              <div className="bg-white rounded-3xl shadow-shadow lg:m-18 md:m-18 sm:m-8 m-8 absolute top-4 left-0 w-full h-full z-10">
                 <div className="text-[#393939] text-lg mt-7 pl-4 pr-4 m-4">
                   <h3 className="flex font-Bold">
-                    <img src={icon} alt="base.svg" className="mr-2" />
+                    <img
+                      src={icon}
+                      alt="base.svg"
+                      className="mr-2 lg:mb-1 mb:mb-1 sm:mb-5 mb-5"
+                    />
                     Submit writing samples made with love
                   </h3>
                   <div className="font-Medium m-7 text-sm text-start mt-2 text-13 max-w-[340px]">
@@ -24,8 +29,8 @@ const Header = () => {
                   </div>
                 </div>
               </div>
-
-              <div className="bg-[#FFE9FF] rounded-3xl mt-32 ml-12 absolute top-1 left-0 w-full h-full">
+              {/* <!-- Second Card --> */}
+              <div className="bg-[#FFE9FF] rounded-3xl mt-32 ml-12 absolute top-6 left-6 md:max-w-[450px] md:max-w-[450px]  sm:max-w-[305px] max-w-[305px]">
                 <div className="text-[#393939] text-lg mt-6 pl-4 pr-4 m-4">
                   <h3 className="flex font-Bold mt-8">
                     <img src={icon} alt="base.svg" className="mr-2" />
