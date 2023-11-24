1a9bcaf20776223d157b99e41e61c1fd1e54ac1d
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-11-24 21:28:25 +0000

    Fixed

--- src/Component/MarqueeText.jsx
+++ src/Component/MarqueeText.jsx
@@ -1,8 +1,6 @@
 const MarqueeText = () => {
   return (
-
-    <div className="overflow-hidden flex flex-col text-justify md:mt-10 mt-[340px]">
-
+    <div className="overflow-hidden flex flex-col text-justify md:mt-10 mt-[340px] hidden lg:inline md:inline">
       <div className="marquee1 font-Bold text-7xl text-zinc-100 leading-72px tracking-widest ">
         <span>
           WRITE <span className="text-emerald-400"> SUBMIT</span> READ .
