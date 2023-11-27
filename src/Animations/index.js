8a0067ab6cbdb57186cc1eedac8004837514135b
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-11-27 12:32:18 +0000

    Additional-Cleanup-for-NavBar

--- src/Animations/index.js
+++ src/Animations/index.js
@@ -7,5 +7,5 @@
 export const slideIn = {
     initial: { x: '-100%' },
     animate: { x: 0 },
-    transition: { duration: 0.7, ease: 'easeOut' }
+    transition: { duration: 0.2, ease: 'easeOut' }
 };
