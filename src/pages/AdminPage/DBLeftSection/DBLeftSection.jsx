ca6af7137f072bfc6b0137b2ac08102831abf653
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-12-18 21:30:47 +0000

    new

--- src/pages/AdminPage/DBLeftSection/DBLeftSection.jsx
+++ src/pages/AdminPage/DBLeftSection/DBLeftSection.jsx
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom'
-import AppLogo from "../../../assets/Images/Logo.svg"
-import { isActiveStyles, isNotActiveStyles } from '../../../utils/styles'
-import { HiUserGroup, HiMiniRectangleGroup } from 'react-icons/hi2';
+import { NavLink } from "react-router-dom";
+import AppLogo from "../../../assets/Images/Logo.svg";
+import { isActiveStyles, isNotActiveStyles } from "../../../utils/styles";
+import { HiUserGroup, HiMiniRectangleGroup } from "react-icons/hi2";
 import { BsSuitClubFill } from "react-icons/bs";
 import { TbWriting } from "react-icons/tb";
 import { MdBubbleChart } from "react-icons/md";
@@ -9,53 +9,90 @@ import { MdCenterFocusStrong } from "react-icons/md";
 
 const DBLeftSection = () => {
   return (
-    <div className='h-full py-12 hidden md:flex flex-col items-center min-w-210 w-[250px] gap-3'>
-           <NavLink to={"/"} className="flex items-center justify-start px-6 gap-4 ">
-            <img
-              src={AppLogo}
-              alt="Logo"
-              className="md:h-[80px] lg:w-[88px] sm:h-[70px]  h-[60px] w-[70px]"
-            />
-          </NavLink>
+    <div className="h-full py-12 hidden md:hidden lg:inline-block md:flex flex-col items-center min-w-210 w-[250px] gap-3">
+      <NavLink to={"/"} className="flex items-center justify-start px-6 gap-4 ">
+        <img
+          src={AppLogo}
+          alt="Logo"
+          className="md:h-[80px] lg:w-[88px] sm:h-[70px]  h-[60px] w-[70px]"
+        />
+      </NavLink>
 
+      <ul className="flex flex-col gap-4 ml-5 mt-10">
+        <NavLink
+          to={"/admin/dashboard"}
+          className={({ isActive }) =>
+            isActive ? `${isActiveStyles}` : isNotActiveStyles
+          }
+        >
+          <MdBubbleChart />
+          Dashboard
+        </NavLink>
 
-          <ul className='flex flex-col gap-4 ml-5 mt-10'>
-            <NavLink to={"/admin/dashboard"} className={({isActive}) => isActive ? `${isActiveStyles}`: isNotActiveStyles}>
-               <MdBubbleChart />
-               Dashboard
-            </NavLink>
+        <NavLink
+          to={"/admin/writers"}
+          className={({ isActive }) =>
+            isActive
+              ? `${isActiveStyles} px-4 py-2 text-bold`
+              : isNotActiveStyles
+          }
+        >
+          <TbWriting />
+          Writers
+        </NavLink>
 
-            <NavLink to={"/admin/writers"} className={({isActive}) => isActive ? `${isActiveStyles} px-4 py-2 text-bold`: isNotActiveStyles}>
-              <TbWriting />
-              Writers
-            </NavLink>
+        <NavLink
+          to={"/admin/clubs"}
+          className={({ isActive }) =>
+            isActive
+              ? `${isActiveStyles} px-4 py-2 text-bold`
+              : isNotActiveStyles
+          }
+        >
+          <BsSuitClubFill />
+          Clubs
+        </NavLink>
 
-            <NavLink to={"/admin/clubs"} className={({isActive}) => isActive ? `${isActiveStyles} px-4 py-2 text-bold`: isNotActiveStyles}>
-              <BsSuitClubFill />
-              Clubs
-            </NavLink>
-            
-            <NavLink to={"/admin/submissions"} className={({isActive}) => isActive ? `${isActiveStyles} px-4 py-2 text-bold`: isNotActiveStyles}>
-              <MdCenterFocusStrong />
-              Submissions
-            </NavLink>
+        <NavLink
+          to={"/admin/submissions"}
+          className={({ isActive }) =>
+            isActive
+              ? `${isActiveStyles} px-4 py-2 text-bold`
+              : isNotActiveStyles
+          }
+        >
+          <MdCenterFocusStrong />
+          Submissions
+        </NavLink>
 
-            <NavLink to={"/admin/team"} className={({isActive}) => isActive ? `${isActiveStyles} px-4 py-2 text-bold`: isNotActiveStyles}>
-               <HiUserGroup />
-               Team
-            </NavLink>
+        <NavLink
+          to={"/admin/team"}
+          className={({ isActive }) =>
+            isActive
+              ? `${isActiveStyles} px-4 py-2 text-bold`
+              : isNotActiveStyles
+          }
+        >
+          <HiUserGroup />
+          Team
+        </NavLink>
 
-            <NavLink to={"/admin/cms"} className={({isActive}) => isActive ? `${isActiveStyles} px-4 py-2 text-bold`: isNotActiveStyles}>
-              <HiMiniRectangleGroup />
-              CMS
-            </NavLink>
-          </ul>
+        <NavLink
+          to={"/admin/cms"}
+          className={({ isActive }) =>
+            isActive
+              ? `${isActiveStyles} px-4 py-2 text-bold`
+              : isNotActiveStyles
+          }
+        >
+          <HiMiniRectangleGroup />
+          CMS
+        </NavLink>
+      </ul>
 
-          <div className=''>
-             {/*Log out section*/}
-          </div>
+      <div className="">{/*Log out section*/}</div>
     </div>
-  )
-}
+  );
+};
 
-export default DBLeftSection
+export default DBLeftSection;
--- src/pages/AdminPage/DBRightSection/DBRightSection.jsx
+++ src/pages/AdminPage/DBRightSection/DBRightSection.jsx
@@ -1,26 +1,26 @@
-import Dashboard from './DashBoard/DashBoard'
-import { Routes, Route } from 'react-router-dom'
-
-
+import Dashboard from "./DashBoard/DashBoard";
+import Team from "../Team/Team";
+import { Routes, Route } from "react-router-dom";
 
 const DBRightSection = () => {
   return (
     <div className="py-12 flex-1 m-2">
-       {/*header*/}
-      <div className='flex flex-col flex-1'> 
-         <Routes>
-           <Route path="/dashboard" element={<Dashboard />} />
-           {/*
+      {/*header*/}
+      <div className="flex flex-col flex-1">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/team" element={<Team />} />
+          {/*
             <Route path="/Writers" element={} />
             <Route path="/Clubs" element={} />
             <Route path="/Submission" element={} />
-            <Route path="/Team" element={} />
+           
             <Route path="/CSM" element={} /> 
             */}
-         </Routes>
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DBRightSection
+export default DBRightSection;
