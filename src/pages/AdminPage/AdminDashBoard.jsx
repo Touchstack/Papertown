56b63b91907cbe951bc88fa20215b1aa952caf20
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-12-24 10:16:57 +0000

    Fixed AdminDashboard height issue and made CSM Homepage responsive

--- src/pages/AdminPage/AdminDashBoard.jsx
+++ src/pages/AdminPage/AdminDashBoard.jsx
@@ -1,15 +1,12 @@
 import DBLeftSection from "./DBLeftSection/DBLeftSection";
 import DBRightSection from "./DBRightSection/DBRightSection";
 
-
 const AdminDashBoard = () => {
   return (
-    <div className='flex w-full h-full bg-gradient-to-b from-[#e7d2de] to-[#f7f4d4]'>
-        <DBLeftSection />
-        <DBRightSection />  
+    <div className="flex bg-gradient-to-b from-[#e7d2de] to-[#f7f4d4] min-h-screen">
+      <DBLeftSection />
+      <DBRightSection />
     </div>
-
-  )
-}
-
-export default AdminDashBoard
+  );
+};
+export default AdminDashBoard;
