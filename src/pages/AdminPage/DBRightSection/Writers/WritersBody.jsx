09cf1867cc06c685434a53df989f35b418c39b43
oddoye <joshoddoye@gmail.com>
2024-01-23 18:26:02 +0000

    Responsiveness for writer page fixed

--- src/pages/AdminPage/DBRightSection/Writers/WritersBody.jsx
+++ src/pages/AdminPage/DBRightSection/Writers/WritersBody.jsx
@@ -9,50 +9,137 @@ const WritersBody = () => {
   }
 
   return (
-    <div>
-        <div className="flex justify-between lg:ml-10 mt-10 border-b-2 border-[#EFEEEE]">
-    <ul className="flex ml-3 md:ml-7 lg:ml-10">
-      <li className=" mr-16">Name</li>
-      <li className=" ">Email</li>
-      <li className="lg:mr-[130%] mr-16 md:mr-[110%]">Contact</li>
-      <li className="lg:mr-[110%] mr-16 md:mr-[10%]">Age</li>
-      <li className="lg:mr-[100%] mr-16 md:mr-[100%]">Grade</li>
-    </ul>
-  </div>
-  <div className="mt-10 lg:ml-10 ml-4">
-    {/* start  */}
-   
-   {Array.from({ length: 12}).map((_, index) => (
-    <div onClick={ handleClick} key={index} className="flex gap-3 text-center items-center mb-5 cursor-pointer">
-      <img src={Profile} alt="" className="w-[51px] h-[51px]"/>
-      <p className=" lg:inline-flex md:inline-flex hidden">
-        Fortune Adeleke
-      </p>
-      <span className="lg:ml-[10%] md:ml-5">
-        fortune.adeleke@gmail.com
-      </span>
-      <span className="lg:ml-[8%] md:ml-16">0200 234 567</span>
-      <span className="lg:ml-[16%] md:ml-[20%]">9</span>
-      <span className="lg:ml-[16%] md:ml-[20%]">Grade 6</span>
-    </div>
-   ))}
-      
-  </div>
-  <div className="lg:ml-[3%] md:ml-[5%] lg:mt-[3%]">
-  <div className="flex items-center justify-between ">
-  <p>
-  <span className="font-Bold opacity-100">1</span>
-  <span className="opacity-75"> of 10 </span>
-  <span className="flex-right text-right ml-[] ">
-   {/*<WritersSlider />*/ }
-    </span>
-</p>
+      <div>
+        <div className="justify center">
+          <div className="w-full h-screen flex items-center justify-center mt-10 overflow-x-auto">
+            <table className="table-auto w-full">
+              <thead>
+                {/*Column Headings*/}
+                <tr className="border-b">
+                  <th className="font-Medium text-[#9A9A9A] text-start px-14 py-2">
+                    Name
+                  </th>
+                  <th className="font-Medium text-[#9A9A9A] text-start px-14 py-2">
+                    Email
+                  </th>
+                  <th className="font-Medium text-[#9A9A9A] text-start px-14 py-2">
+                    Contact
+                  </th>
+                  <th className="font-Medium text-[#9A9A9A] text-start px-14 py-2">
+                    Age
+                  </th>
+                  <th className="font-Medium text-[#9A9A9A] text-start px-14 py-2">
+                   Grade
+                  </th>
+                </tr>
+              </thead>
+
+              {/*Headings end*/}
+              <tbody>
+                <tr className="bg-[#F9F9FB] border-gray-300 rounded-md" onClick={handleClick}>
+                  <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr className="">
+                  <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                  
+                </tr>
+                {/*NewRow*/}
+                <tr className="bg-[#F9F9FB] border-gray-300 rounded-md">
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr>
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr className="bg-[#F9F9FB] border-gray-300 rounded-md">
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr>
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr className="bg-[#F9F9FB] border-gray-300 rounded-md">
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+                <tr>
+                <td className="font-Medium lg:px-14 px-8 py-5">
+                    <img src={Profile} alt="" className="w-[51px] h-[51px] inline-block align-middle mr-2"/> Fortune Adeleke
+                    </td>
+                  <td className="font-Medium px-14 py-5">
+                    fortune.adeleke@gmail.com
+                  </td>
+                  <td className="font-Medium px-14 py-5">0200 234 567</td>
+                  <td className="font-Medium px-14 py-5">9</td>
+                  <td className="font-Medium px-14 py-5">Grade 6</td>
+                </tr>
+                {/*NewRow*/}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
- 
-  </div>
-  </div>
-);
-}
+    );
+  };
 
 
 export default WritersBody
