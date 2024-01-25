a2dfc738673a84198d32d323babbc354cbeabd13
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2024-01-25 12:58:33 -0800

    Implement Logout feature for PaperTown
    Implement Logout feature for PaperTown

--- src/pages/Slidershow/OverLay.jsx
+++ src/pages/Slidershow/OverLay.jsx
@@ -1,7 +1,6 @@
 import BGimage from "../../assets/Images/image.png";
 import { HiMiniChevronLeft, HiMiniChevronRight } from "react-icons/hi2";
 import { partners } from "../../../ConstantData";
-
 const OverLay = () => {
   return (
     <div className="relative mt-[6rem] flex flex-col items-center z-50 px-1">
@@ -10,31 +9,46 @@ const OverLay = () => {
         <img
           src={BGimage}
           alt="Group of students"
-          className="w-[90vw]  md:h-[501px] h-[670px] rounded-[30px] object-cover"
+          className="w-[90vw] md:h-[600px] h-[770px] rounded-[30px] object-cover"
         />
       </div>
 
       {/* Overlay content */}
-      <div className="absolute md:bottom-[0rem] w-full h-full p-4 flex flex-col justify-between">
+      <div className="absolute md:bottom-[0rem] w-full h-full p-6 flex flex-col justify-between">
         {/* Title and navigation on the same line */}
         <div className="flex justify-between items-center w-full pt-4 md:pt-8">
           <p className="text-white text-[42px] font-bold">Our Partners</p>
           <div className="md:flex hidden">
-            <button aria-label="Previous" className="rounded-full border border-[#EEF7F7] h-[30px] w-[30px] flex items-center justify-center">
-              <HiMiniChevronLeft className="text-[20px] text-white"/>
+            <button
+              aria-label="Previous"
+              className="rounded-full border border-[#EEF7F7] h-[40px] w-[40px] flex items-center justify-center"
+            >
+              <HiMiniChevronLeft className="text-[40px] text-white" />
             </button>
-            <button aria-label="Next" className="rounded-full border border-[#EEF7F7] h-[30px] w-[30px] flex items-center justify-center ml-2">
-              <HiMiniChevronRight className="text-[20px] text-white"/>
+            <button
+              aria-label="Next"
+              className="rounded-full border border-[#EEF7F7] h-[40px] w-[40px] flex items-center justify-center ml-2"
+            >
+              <HiMiniChevronRight className="text-[40px] text-white" />
             </button>
           </div>
         </div>
-        
+
         {/* Partner cards container */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center w-full  py-4 md:py-8 my-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center w-full py-4 md:py-0 my-auto ">
           {partners.map((partner) => (
-            <div key={partner.name} className="bg-white md:p-[12px] p-2 shadow-md rounded-[30px] md:h-[187px]">
-              <img src={partner.logo} alt={`${partner.name} logo`} className="h-5" />
-              <p className="text-left text-[24px] font-bold pb-5">{partner.name}</p>
+            <div
+              key={partner.name}
+              className="bg-white md:p-[30px] p-6 shadow-md rounded-[30px] md:h-[210px]"
+            >
+              <img
+                src={partner.logo}
+                alt={`${partner.name} logo`}
+                className="h-5"
+              />
+              <p className="text-left text-[24px] font-bold pb-5">
+                {partner.name}
+              </p>
               <p className="text-left">{partner.description}</p>
             </div>
           ))}
@@ -42,6 +56,6 @@ const OverLay = () => {
       </div>
     </div>
   );
-}
+};
 
 export default OverLay;
--- src/pages/Slidershow/PatnersComponent.jsx
+++ src/pages/Slidershow/PatnersComponent.jsx
@@ -1,22 +1,20 @@
 import Marquee from "react-fast-marquee";
 import OverLay from "./OverLay";
 const PatnersComponent = () => {
-  
-
   return (
     <div className="flex justify-center mb-10">
-        {/*MArquee Div*/}
-        <div className="absolute gap-10  z-10">
-              <Marquee>
-              <p className="marqueeText text-[50px]  md:text-[128px] font-Bold text-left font-[800] opacity-[0.20]">
-                SOME OF OUR WRITING SAMPLES
-              </p>
-              </Marquee>   
-         </div>
+      {/*MArquee Div*/}
+      <div className="absolute gap-10  z-10">
+        <Marquee>
+          <p className="marqueeText text-[50px] text-[#F2F2F2] md:text-[128px] font-Bold text-left font-[800] opacity-[0.20]">
+            SOME OF OUR WRITING SAMPLES
+          </p>
+        </Marquee>
+      </div>
 
-           <OverLay />
+      <OverLay />
     </div>
-  )
-}
+  );
+};
 
-export default PatnersComponent
+export default PatnersComponent;
