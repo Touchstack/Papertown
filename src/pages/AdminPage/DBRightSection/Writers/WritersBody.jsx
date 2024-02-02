f02f8ebeb5252b472fc8ac7a3d6d822725a0730c
oddoye <joshoddoye@gmail.com>
2024-02-02 01:57:27 +0000

    Add new writer & submission responsiveness fixed

--- src/pages/AdminPage/DBRightSection/Writers/WritersBody.jsx
+++ src/pages/AdminPage/DBRightSection/Writers/WritersBody.jsx
@@ -9,8 +9,8 @@ const WritersBody = () => {
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="ml-[100px] text-md text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+        <thead className="ml-[100px] text-md text-gray-700 bg-gray-50  ">
           <tr className="border-b">
             <th scope="col" className="px-6 py-3">Name</th>
             <th scope="col" className="px-6 py-3">Email</th>
