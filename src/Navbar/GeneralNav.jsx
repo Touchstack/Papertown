d60d94acb2416686e55c742fb58568e88eaeb55f
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2024-04-30 18:36:43 +0000

    Updated General Nabar

--- src/Navbar/GeneralNav.jsx
+++ src/Navbar/GeneralNav.jsx
@@ -105,16 +105,17 @@ const  GeneralNav = ({ color, btnColor, bgColor, bgShadow }) => {
         <nav
           className={`flex md:container mx-auto px-2 text-center font-VarelaRegular rounded-full max-w-6xl lg:pt-2 md:pt-2 sm:px-4 py-3 ${bgColorClass} ${bgShadowClass}`}
         >
-          <div className="md:container md:pl-25 flex justify-between items-center m-auto">
+          <div className="container px-5 md:pl-25 flex justify-between items-center m-auto">
             <a
               href="/"
               className="transition duration-500 ease-in-out hover:scale-110 flex items-center"
             >
-              <img
-                src={AppLogo}
-                alt="Logo"
-                className="md:h-[80px] lg:w-[88px] sm:h-[70px]  h-[60px] w-[70px]"
-              />
+              <div className="md:h-[80px] lg:w-[88px] sm:h-[70px]  h-[65px] w-[65px]">
+                <img
+                  src={AppLogo}
+                  alt="Logo"
+                />
+              </div>
             </a>
             <div
               className={`lg:flex justify-between w-full md:w-auto `}
